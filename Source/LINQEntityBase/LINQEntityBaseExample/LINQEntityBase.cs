7698450443803ddf8bdecaab926cc2b28bbebd5c
SND\mhunter_cp <SND\mhunter_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2008-03-12 20:55:45 +0000

    git-svn-id: https://linq2sqleb.svn.codeplex.com/svn@111 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ Source/LINQEntityBase/LINQEntityBaseExample/LINQEntityBase.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+namespace LINQEntityBaseExample
+{
+    /// <summary>
+    /// This object should be used as a superclass for LINQ to SQL Entities
+    /// which are disconnected and need change tracking capabilties.
+    /// 
+    /// To add this class as a superclass for all entities in a DataContext
+    /// Edit the dbml file for the DataContext and add in the "<Database>" xml element
+    /// the "EntityBase" xml attribute, then save the dbml file, right click the dbml file in
+    /// the VS project and select "Run Custom Tool".
+    /// 
+    /// e.g. EntityBase="LINQEntityBaseExample.LINQEntityBase"
+    /// 
+    /// Note, for this to work Child entities should have property a RowVersion/Timestamp field (can be named anything)
+    /// </summary>    
+    public abstract class LINQEntityBase
+    {
+        #region constructor
+
+        /// <summary>
+        /// Default constructor, always called as long as child entity was created with default constructure aswell.
+        /// </summary>
+        protected LINQEntityBase()
+        {
+            FindImportantProperties();
+            BindToEntityEvents();
+            _entityHierarchy = new EntityHierarchy(this, _entityAssociationProperties);
+        }
+
+        #endregion constructor
+
+        #region private_members
+
+        private bool _isModified = false; //whether or not a property has been changed
+        private bool _isDeleted = false; //indicates if the record should be deleted        
+        private string _entityGUID = Guid.NewGuid().ToString(); //a unique identifier for the entity
+        private PropertyInfo _entityVersionProperty; // stores the property info for the row stamp field.
+        private Dictionary<string, PropertyInfo> _entityAssociationProperties = new Dictionary<string, PropertyInfo>(); // stores the property info for associations
+        private Dictionary<string, PropertyInfo> _entityAssociationFKProperties = new Dictionary<string, PropertyInfo>(); // stores the property info for foreingKey associations
+        private EntityHierarchy _entityHierarchy; //used to hold the private class that allows entity hierarchy to be enumerated
+
+        /// <summary>
+        /// Returns an ID that is unique for this object.
+        /// </summary>
+        public string LINQEntityGUID
+        {
+            get { return _entityGUID; }
+        }
+
+        /// <summary>
+        /// This method binds to the events of the entity that are required.
+        /// </summary>
+        private void BindToEntityEvents()
+        {
+            INotifyPropertyChanged childEntity;
+            childEntity = (INotifyPropertyChanged)this;
+
+            // bind the IsModified event, so when a property is changed the base class is aware of it.
+            childEntity.PropertyChanged += new PropertyChangedEventHandler(PropertyChanged);
+
+        }
+
+        /// <summary>
+        /// Loops through the available properties on the class and gets the rowVersion and association properties
+        /// </summary>
+        private void FindImportantProperties()
+        {
+            ColumnAttribute columnAttribute;
+            AssociationAttribute assocAttribute;
+
+            foreach (PropertyInfo propInfo in this.GetType().GetProperties())
+            {
+                // check it's an association attribute first
+                assocAttribute = (AssociationAttribute)Attribute.GetCustomAttribute(propInfo, typeof(AssociationAttribute), false);
+
+                // if it is an association attribute
+                if (assocAttribute != null)
+                {
+                    // Store the FK relationships seperately (i.e. child to parent relationships);
+                    if (assocAttribute.IsForeignKey != true)
+                        _entityAssociationProperties.Add(propInfo.Name, propInfo);
+                    else
+                        _entityAssociationFKProperties.Add(propInfo.Name, propInfo);
+
+                }
+                else if (_entityVersionProperty == null)
+                {
+                    // it wasn't an association, check to see it's a RowVersion (if we haven't already found it).
+                    columnAttribute = (ColumnAttribute)Attribute.GetCustomAttribute(propInfo, typeof(ColumnAttribute), false);
+                    if (columnAttribute != null && columnAttribute.IsVersion == true)
+                    {
+                        _entityVersionProperty = propInfo;
+                    }
+                }
+            }
+
+            // By the end of this process, if we haven't found the row version property, complain.
+            if (_entityVersionProperty == null)
+                throw new ApplicationException("No RowVersion Property Defined, you must include a version property.");
+        }
+
+        /// <summary>
+        /// Handles the property changed event sent from child object.
+        /// </summary>
+        /// <param name="sender">child object</param>
+        /// <param name="e">Property Changed arguements</param>
+        private void PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // if the property being changed is the timestamp field
+            // assume this property is being set after a submit changes.
+            if (e.PropertyName == _entityVersionProperty.Name)
+            {
+                // the object can be assumed to be no longer in a modified or deleted state
+                // so reset the flag.  IsNew does not need to be set because this is dynamically evaluated.
+                _isModified = false;
+                _isDeleted = false;
+            }
+            else
+            {
+                // Check if it's new and it's an association. 
+                // If the entity is new, it's new not modified so ignore.
+                // If the property is an association then mark the entity as modified.
+                if (!IsNew && 
+                    !_entityAssociationProperties.ContainsKey(e.PropertyName) &&
+                    !_entityAssociationFKProperties.ContainsKey(e.PropertyName))
+                    
+                {
+                    _isModified = true;
+                }
+            }
+        }
+
+
+        #endregion private_members
+
+        #region public_members
+
+        /// <summary>
+        /// Returns if the entity is new or not
+        /// </summary>
+        public bool IsNew
+        {
+            get
+            {
+                // If the timestamp is zero bytes then it's new.
+                return _entityVersionProperty.GetValue(this, null) == null;
+            }
+        }
+
+        /// <summary>
+        /// Returns if the property is modified or allows caller to set if the
+        /// object is modified.
+        /// </summary> 
+        public bool IsModified
+        {
+            get
+            {
+                // If it's been marked as modified, and it's not new then it's considered modified.
+                return _isModified == true && IsNew == false;
+            }
+            set
+            {
+                // If the caller want's to specifically attempt to modify for
+                // some reason or we are re-hydrating the object from serialization
+                // allow _isModified to be set manually
+                _isModified = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns/Sets if the object has been marked for deletion.
+        /// </summary>
+        public bool IsDeleted
+        {
+            get
+            {
+                return _isDeleted;
+            }
+            set
+            {
+                // Allow a user to specifically set that an object is deleted.
+                // as this object doesn't delete deleted objects.
+                _isDeleted = value;
+            }
+        }
+
+        /// <summary>
+        /// This method flattens the hierachy of objects into a single list that can be queried by linq
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<LINQEntityBase> GetEntityHierarchy()
+        {
+            return (from t in _entityHierarchy
+                    select t);
+        }
+
+
+        /// <summary>
+        /// Syncronises this EntityBase and all sub objects
+        /// with a data context.
+        /// </summary>
+        /// <param name="targetDataContext">The data context that will apply the changes</param>
+        public void SynchroniseWithDataContext(DataContext targetDataContext)
+        {
+            foreach (LINQEntityBase entity in this.GetEntityHierarchy())
+            {
+                if (!entity.IsNew && !entity.IsModified)
+                    targetDataContext.GetTable(entity.GetType()).Attach(entity, false);
+                else if (entity.IsNew)
+                    targetDataContext.GetTable(entity.GetType()).InsertOnSubmit(entity);
+                else if (entity.IsModified)
+                    targetDataContext.GetTable(entity.GetType()).Attach(entity, true);
+
+                if (entity.IsDeleted)
+                    targetDataContext.GetTable(entity.GetType()).DeleteOnSubmit(entity);
+            }
+        }
+
+        #endregion public_members
+
+        #region private_classes
+
+        /// <summary>
+        /// This class is used internally to implement IEnumerable, so that the hierarchy can
+        /// be enumerated by LINQ queries.
+        /// </summary>
+        private class EntityHierarchy : IEnumerable<LINQEntityBase>
+        {
+            private Dictionary<string, PropertyInfo> _entityAssociationProperties;
+            private LINQEntityBase _entityRoot;
+
+            public EntityHierarchy(LINQEntityBase EntityRoot, Dictionary<string, PropertyInfo> EntityAssociationProperties)
+            {
+                _entityRoot = EntityRoot;
+                _entityAssociationProperties = EntityAssociationProperties;
+            }
+
+            // implement the GetEnumerator Type
+            public IEnumerator<LINQEntityBase> GetEnumerator()
+            {
+                Type entityType;
+
+                // return the current object
+                yield return _entityRoot;
+
+                // return the children (using reflection)
+                foreach (PropertyInfo propInfo in _entityAssociationProperties.Values)
+                {
+                    // Is it an EntitySet<>?
+                    if (propInfo.PropertyType.IsGenericType && propInfo.PropertyType.GetGenericTypeDefinition() == typeof(EntitySet<>))
+                    {
+                        // It's an EntitySet<> so lets grab the value, loop through each value and
+                        // return each value as an EntityBase.
+                        foreach (var entity in propInfo.GetValue(_entityRoot, null) as IEnumerable)
+                        {
+                            entityType = entity.GetType();
+
+                            if (entityType.IsSubclassOf(typeof(LINQEntityBase)))
+                            {
+                                //Ask for these children for their section of the tree.
+                                foreach (LINQEntityBase subEntity in (entity as LINQEntityBase).GetEntityHierarchy())
+                                {
+                                    yield return subEntity;
+                                }
+                            }
+                        }
+                    }
+                    else if (propInfo.PropertyType.IsSubclassOf(typeof(LINQEntityBase)))
+                    {
+                        //Ask for these children for their section of the tree.
+                        foreach (LINQEntityBase subEntity in (propInfo.GetValue(_entityRoot, null) as LINQEntityBase).GetEntityHierarchy())
+                        {
+                            yield return subEntity;
+                        }
+                    }
+                }
+
+            }
+
+            // implement the GetEnumerator type
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+
+        }
+
+        #endregion
+
+
+    }
+
+}
---
+++ Source/LINQEntityBase/LINQEntityBaseExample/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+
+namespace LINQEntityBaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXAMPLE: 
+            // 1. Detach From Original
+            // 2. Add/Modify
+            // 3. Re-Attach to new DataContext + Commit
+            // 4. Delete Record, 
+            // 5. Re-Attach to new DataContext + Commit
+
+            // NOTES:
+            // Hierarchy is :
+            // Customer --> Order --> Order Detail
+            // Product is not updateable because it is not in hierarchy, 
+            // as it is only referenced.
+
+            List<Customer> customers;
+
+            // Note that each table has a RowVersion field and DeferredLoading is off.
+            using (NorthWindDataContext db = new NorthWindDataContext())
+            {
+                DataLoadOptions lo = new DataLoadOptions();
+                lo.LoadWith<Customer>(c => c.Orders);
+                lo.LoadWith<Order>(o => o.Order_Details);
+                lo.LoadWith<Order_Detail>(od => od.Product);
+                db.LoadOptions = lo;
+                db.DeferredLoadingEnabled = false; 
+                customers = (from c in db.Customers
+                            where c.CustomerID == "ALFKI"
+                            select c).ToList();
+
+            }
+
+            /// WE ARE NOW DISCONNECTED ///
+
+            // Update Customer
+            string fax = customers.First().Fax;
+            string phone = customers.First().Phone;
+            customers.First().Fax = phone;
+            customers.First().Phone = fax;
+            Console.WriteLine("Customer Modified: {0}", customers.First().LINQEntityGUID);
+
+            // Add an order record
+            Order orderAdded = new Order()
+                    {EmployeeID = 3, 
+                     OrderDate = DateTime.Now,
+                     RequiredDate = DateTime.Now.AddDays(30),
+                     ShippedDate  = DateTime.Now.AddDays(10),
+                     ShipVia = 1,
+                     Freight = 10,
+                     ShipName = "Delete Me Later!",
+                     ShipAddress = "Obere Str. 57",
+                     ShipCity = "Berlin",
+                     ShipRegion = null,
+                     ShipPostalCode = "12209",
+                     ShipCountry = "Germany"};
+            
+            customers.First().Orders.Add(orderAdded);
+
+
+            Console.WriteLine("Order Added: {0}", orderAdded.LINQEntityGUID);
+
+            // Modify Some Orders
+            List<Order> orders = (from o in customers.First().Orders
+                                 where o.ShipCountry == "Germany"
+                                 select o).ToList();
+            
+            foreach (Order order in orders)
+            {
+                // $1 to frieght charge
+                order.Freight += 1;
+                Console.WriteLine("Order Modified: {0}", order.LINQEntityGUID);
+            }
+
+
+
+            // Update Order via root object, creating a new data context
+            using(NorthWindDataContext db = new NorthWindDataContext())
+            {
+                Console.WriteLine("---------------");
+                Console.WriteLine("Changes Tracked");
+                Console.WriteLine("---------------");
+                foreach (LINQEntityBase entity in customers.First().GetEntityHierarchy())
+                {
+                    if (entity.IsNew)
+                        Console.WriteLine("--> Added: {0}", entity.LINQEntityGUID);
+                    else if (entity.IsModified)
+                        Console.WriteLine("--> Modified {0}", entity.LINQEntityGUID);
+                    else if (entity.IsDeleted)
+                        Console.WriteLine("-->Deleted {0}", entity.LINQEntityGUID);
+
+                }
+
+                db.DeferredLoadingEnabled = false;
+                customers.First().SynchroniseWithDataContext(db);
+                db.SubmitChanges();
+                Console.WriteLine("----------------");
+                Console.WriteLine("Changes Comitted");
+                Console.WriteLine("----------------");
+            }
+
+            /// WE ARE NOW DISCONNECTED AGAIN! ///
+
+            // Delete an order (NOTE: it's using the flat list!)
+            List<Order> ordersDelete = (from o in customers.First().GetEntityHierarchy().OfType<Order>()
+                                       where o.ShipName == "Delete Me Later!"
+                                       select o).ToList();
+
+            foreach (Order orderDeleted in ordersDelete)
+            {
+                orderDeleted.IsDeleted = true;
+                Console.WriteLine("Order Deleted: {0}", orderDeleted.LINQEntityGUID);
+            }
+
+            // Update Order via root object, creating a new data context
+            using (NorthWindDataContext db = new NorthWindDataContext())
+            {
+                Console.WriteLine("---------------");
+                Console.WriteLine("Changes Tracked");
+                Console.WriteLine("---------------");
+                foreach (LINQEntityBase entity in customers.First().GetEntityHierarchy())
+                {
+                    if (entity.IsNew)
+                        Console.WriteLine("--> Added: {0}", entity.LINQEntityGUID);
+                    else if (entity.IsModified)
+                        Console.WriteLine("--> Modified {0}", entity.LINQEntityGUID);
+                    else if (entity.IsDeleted)
+                        Console.WriteLine("-->Deleted {0}", entity.LINQEntityGUID);
+
+                }
+                customers.First().SynchroniseWithDataContext(db);
+                db.SubmitChanges();
+                Console.WriteLine("---------------");
+                Console.WriteLine("Changes Comitted");
+                Console.WriteLine("---------------");
+            }
+
+            Console.ReadKey();
+            Order_Detail fred = new Order_Detail();
+        }
+
+    }
+}
